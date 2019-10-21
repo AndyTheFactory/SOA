099d3bc4b3f397874e4783df3b656460a32bbfb5
Andrei <andrei@thephpfactory.com>
2019-10-22 01:27:46 +0300

    kk

--- Homew3/homew3.py
+++ Homew3/homew3.py
@@ -1,35 +1,49 @@
 import numpy as np
 import random
 import sys
+import tqdm
 
 global G1, G2, G3
 
 
 def init_array(size):
-    A = np.random.randint(100, size=(9, 9))
+    A = np.random.randint(low=1, high=100, size=(size, size))
     np.fill_diagonal(A, 0)
     A = np.triu(A) + np.tril(A.T)
     return A
 
 
-def TSP(Graph, S, destination):
-    if (len(S) == 2):
-        lS = list(S)
-        return list([(1, destination)]), Graph[lS[0], lS[1]]
+def TSP_ph1(Graph, S, destination):
+    if (len(S) <= 0):
+        # s=list(S)
+        return list([(1, destination)]), Graph[0, destination - 1]
     else:
-        min_dist = 999
-        min_node = -1
+        min_dist = sys.maxsize
         min_way = list()
+        S = S.difference({destination})
         for _, node in enumerate(S):
-            if (node != 1 and node != destination):
-                w, d = TSP(Graph, S.difference({node}), node)
-                d += Graph[node - 1, destination - 1]
-                if d < min_dist:
-                    min_dist = d
-                    min_node = node
-                    min_way = w
-                    min_way.append((node, destination))
+            w, d = TSP_ph1(Graph, S.difference({node}), node)
+            d += Graph[node - 1, destination - 1]
+            if d < min_dist:
+                min_dist = d
+                min_way = w
+                min_way.append((node, destination))
         return min_way, min_dist
+
+
+def TSP(Graph):
+    nodecount = len(Graph[0])
+    S = set(range(2, nodecount + 1))
+    min_dist = sys.maxsize
+    min_way = list()
+    for _, node in enumerate(S):
+        w, d = TSP_ph1(Graph, S, node)
+        d += Graph[node - 1, 0]
+        if d < min_dist:
+            min_dist = d
+            min_way = w
+            min_way.append((node, 1))
+    return min_way, min_dist
 
 
 class Ant:
@@ -37,44 +51,48 @@ class Ant:
         self.colony = colony
         self.tabu = list()
         self.all_nodes = set()
-        self.position = 0
+        self.position = -1
         self.L = 0
 
     def probability(self, j):
         if j in self.tabu:
             return 0
-        denominator = 0.0
-        for k in self.all_nodes - set(self.tabu):
+        denominator = sys.float_info.epsilon
+        for k in self.all_nodes.difference(set(self.tabu)):
             denominator += self.colony.tau[self.position, k] ** self.colony.alpha * \
                            (1 / self.colony.Graph[self.position, k]) ** self.colony.beta
         p: float
         p = self.colony.tau[self.position, j] ** self.colony.alpha * \
             (1 / self.colony.Graph[self.position, j]) ** self.colony.beta
-        return p
+        return p / denominator
 
     def move(self):
         prob = [self.probability(x) for x in self.all_nodes]
-        nextpos = np.random.choice(self.all_nodes, p=prob)
+        s = sum(prob)
+        if s != 1.:
+            prob = [x / s for x in prob]
+        nextpos = np.random.choice(list(self.all_nodes), p=prob)
         self.tabu.append(nextpos)
         self.L += self.colony.Graph[self.position, nextpos]
-        self.position=nextpos
+        self.position = nextpos
         return nextpos
 
     def close_circuit(self):
-        if self.all_nodes - set(self.tabu) == set():
+        if self.all_nodes.difference(set(self.tabu)) == set():
             self.L += self.colony.Graph[self.position, self.tabu[0]]
 
     def get_delta_tau(self):
-        delta_tau_k = np.zeros(self.colony.Graph.shape)
-        for i,node in enumerate(self.tabu):
-            next_node = self.tabu[i+1] if i < len(self.tabu)-1 else self.tabu[0]
+        delta_tau_k = np.zeros(self.colony.Graph.shape).astype(float)
+        for i, node in enumerate(self.tabu):
+            next_node = self.tabu[i + 1] if i < len(self.tabu) - 1 else self.tabu[0]
             delta_tau_k[node, next_node] = AntColony.Q / self.L
             delta_tau_k[next_node, node] = AntColony.Q / self.L
+        return delta_tau_k
 
 
 class AntColony:
     q = 1.5
-    Q = 0.1
+    Q = 100
     nr_ants = 5
     N_MAX = 1000
     TAU_ZERO = 1e-5
@@ -96,12 +114,12 @@ class AntColony:
         self.nr_nodes = Graph.shape[0]
         for ant in self.ants:
             while True:
-                pos = random.randint(0, self.nr_nodes-1)  # Nodes are 1..N , Graph is 0..N-1
+                pos = random.randint(0, self.nr_nodes - 1)  # Nodes are 1..N , Graph is 0..N-1
                 if not self.checkPosition(pos):
                     break
             ant.position = pos
             ant.tabu.append(pos)
-            ant.all_nodes = set(range(1, self.nr_nodes + 1))
+            ant.all_nodes = set(range(0, self.nr_nodes))
 
     def checkPosition(self, node):
         for ant in self.ants:
@@ -114,22 +132,24 @@ class AntColony:
         min_way = list()
         for ant in self.ants:
             if ant.L < min:
-                min_way=ant.tabu
-                min=ant.L
+                min_way = ant.tabu
+                min = ant.L
 
         return min_way, min
 
     def checkStagnation(self):
-        prev=None
+        prev = None
         for ant in self.ants:
-            if prev==None:
+            if prev == None:
+                prev = ant
                 continue
-            if prev.L != ant.L or not np.array_equal(np.array(prev.tabu),np.array(ant.tabu)):
+            if prev.L != ant.L or not np.array_equal(np.array(prev.tabu), np.array(ant.tabu)):
                 return False
+            prev = ant
         return True
 
     def move_all_ants(self):
-        for n in range(0, self.nr_nodes):
+        for n in range(0, self.nr_nodes - 1):  # -1 because we initalize the ants with one node
             for ant in self.ants:
                 ant.move()
         for ant in self.ants:
@@ -139,14 +159,22 @@ class AntColony:
         for ant in self.ants:
             self.delta_tau += ant.get_delta_tau()
 
+    def reset_ants(self):
+        for ant in self.ants:
+            ant.position = ant.tabu[0]
+            ant.tabu = [ant.position]
+            ant.L = 0
+
     def run(self):
         n_iteration = 0
         best_way = list()
         best_cost = sys.maxsize
+        pbar = tqdm.tqdm(total=self.N_MAX)
+
         while True:
             self.move_all_ants()
             way, cost = self.getBestCircuit()
-            if cost<best_cost:
+            if cost < best_cost:
                 best_cost = cost
                 best_way = way
 
@@ -154,17 +182,43 @@ class AntColony:
 
             self.tau = self.tau * self.rho + self.delta_tau
             n_iteration += 1
+            pbar.update(1)
 
-            self.delta_tau = np.zeros(self.Graph.shape)
+            self.delta_tau = np.zeros(self.Graph.shape).astype(float)
 
             if n_iteration >= self.N_MAX or self.checkStagnation():
                 break
+            self.reset_ants()
 
-        return best_way, best_cost
+        pbar.close()
+        res_way = []
+        p = None
+        for n in best_way:
+            if p == None:
+                p = n
+                continue
+            res_way.append((p + 1, n + 1))
+            p = n
+        res_way.append((best_way[-1] + 1, best_way[0] + 1))
+        return res_way, best_cost
 
 
 if __name__ == "__main__":
     G1 = init_array(9)
-    print(TSP(G1,set(range(1,9)),9))
-    ac = AntColony(1, 1, 1)
+    # G1 = np.array([[0, 43, 18, 34, 67, 41, 32, 1, 84],
+    #                [43, 0, 93, 51, 1, 86, 44, 92, 27],
+    #                [18, 93, 0, 49, 33, 87, 36, 22, 2],
+    #                [34, 51, 49, 0, 18, 9, 24, 19, 28],
+    #                [67, 1, 33, 18, 0, 89, 85, 64, 57],
+    #                [41, 86, 87, 9, 89, 0, 27, 9, 5],
+    #                [32, 44, 36, 24, 85, 27, 0, 42, 10],
+    #                [1, 92, 22, 19, 64, 9, 42, 0, 44],
+    #                [84, 27, 2, 28, 57, 5, 10, 44, 0]])
+
+    print(TSP(G1))
+
+    ac = AntColony(0.5, 1, 0.3)
     ac.loadGraph(G1)
+    print("AC Result: ")
+    way, cost = ac.run()
+    print(way, cost)
